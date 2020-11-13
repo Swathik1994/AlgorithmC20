56f6649e8b2b3262a208ede779fb4d49b5d011a0
Swathik1994 <70898286+Swathik1994@users.noreply.github.com>
2020-11-13 14:39:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AlgorithmC20
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,42 @@
+var movingR,fixedR;
+
+function setup() {
+  createCanvas(1200,800);
+
+movingR= createSprite(200, 400, 100, 40);
+movingR.shapeColor="green";
+movingR.debug=true;
+
+fixedR=createSprite(400,400,40,100);
+fixedR.shapeColor="green";
+fixedR.debug=true;
+
+
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  
+  movingR.x=World.mouseX;
+  movingR.y=World.mouseY;
+
+  //250-200<20+50
+
+if(movingR.x-fixedR.x < fixedR.width/2+movingR.width/2 && 
+  
+  movingR.x-fixedR.x < fixedR.height/2+movingR.height/2 ){
+
+  movingR.shapeColor="blue";
+  fixedR.shapeColor="blue";
+}
+
+else{
+  movingR.shapeColor="green";
+  fixedR.shapeColor="green";
+
+}
+ 
+  drawSprites();
+}
+
